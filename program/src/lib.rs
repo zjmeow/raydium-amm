0dd34a015a127e97f07a2cbe8e0bb00054a94ccf
zjmeow <zjmeow@gmail.com>
2025-02-08 23:45:11 -0800

    改成可依赖库

--- program/src/lib.rs
+++ program/src/lib.rs
@@ -3,8 +3,6 @@
 //! An Uniswap-like program for the Solana blockchain.
 #[macro_use]
 pub mod log;
-
-mod entrypoint;
 pub mod error;
 pub mod instruction;
 pub mod invokers;
@@ -12,21 +10,5 @@ pub mod math;
 pub mod processor;
 pub mod state;
 
-// Export current solana-sdk types for downstream users who may also be building with a different solana-sdk version
 pub use solana_program;
 
-#[cfg(not(feature = "no-entrypoint"))]
-solana_security_txt::security_txt! {
-    name: "raydium-amm",
-    project_url: "https://raydium.io",
-    contacts: "link:https://immunefi.com/bounty/raydium",
-    policy: "https://immunefi.com/bounty/raydium",
-    source_code: "https://github.com/raydium-io/raydium-amm",
-    preferred_languages: "en",
-    auditors: "https://github.com/raydium-io/raydium-docs/blob/master/audit/MadSheild%20Q2%202023/Raydium%20updated%20orderbook%20AMM%20program%20%26%20OpenBook%20migration.pdf"
-}
-
-#[cfg(feature = "devnet")]
-solana_program::declare_id!("HWy1jotHpo6UqeQxx49dpYYdQB8wj9Qk9MdxwjLvDHB8");
-#[cfg(not(feature = "devnet"))]
-solana_program::declare_id!("675kPX9MHTjS2zt1qfr1NYHuzeLXfQM9H24wFSUt1Mp8");
